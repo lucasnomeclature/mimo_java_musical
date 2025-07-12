6a91593005f80f69e5ad6acce0d73aa42e688c38
lucasnomeclature <diversetdechet@gmail.com>
2025-07-12 17:38:36 +0200

    Add files via upload
    premières modifs avec pierre

--- java-mimo-2025/src/main/java/edu/mimo/books/entity/Author.java
+++ java-mimo-2025/src/main/java/edu/mimo/books/entity/Author.java
@@ -13,16 +13,10 @@ public class Author {
     @Column(name = "name")
     private String name;
 
-    @ManyToOne 
-    @JoinColumn(name = "country_id")
-    private Country country;
-
     public Integer getId() { return id; }
     public void setId(Integer id) { this.id = id; }
 
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 
-    public Country getCountry() { return country; }
-    public void setCountry(Country country) { this.country = country; }
 } 
--- java-mimo-2025/src/main/java/edu/mimo/books/repository/BookRepository.java
+++ java-mimo-2025/src/main/java/edu/mimo/books/repository/BookRepository.java
@@ -1,18 +1,18 @@
 package edu.mimo.books.repository;
 
-import edu.mimo.books.entity.Book;
+import edu.mimo.books.entity.Musical;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface BookRepository extends JpaRepository<Book, Integer> {
+public interface BookRepository extends JpaRepository<Musical, Integer> {
 
     //@EntityGraph(attributePaths = {"author"})
-    Optional<Book> findById(Integer id);
+    Optional<Musical> findById(Integer id);
 
-    List<Book> findByAuthorId(Integer authorId);
+    List<Musical> findByAuthorId(Integer authorId);
 
 
 
